88e9159ec398f5e29d129c3e99b39428a0ad7020
Nazar <kaminskyi.nazar.1923@gmail.com>
2018-04-22 18:14:37 +0300

    hz11

--- test.py
+++ test.py
@@ -4,6 +4,18 @@ import pandas as pd
 import numpy as np
 
 
+# def t_0(x):
+#     return -1.0 + 2.0 * x
+#
+#
+# def t_1(x):
+#     return 1.0 - 8.0 * x + 8.0 * (x**2)
+#
+#
+# def t_2(x):
+#     return -1 + 18 * x - 48 * (x**2) + 32 * (x**3)
+
+
 def t_0(x):
     return 1.0
 
@@ -15,54 +27,61 @@ def t_1(x):
 def t_2(x):
     return 1.0 - 8.0 * x + 8.0 * (x**2)
 
-# new_df = pd.read_csv('table_51_b.csv', index_col='Unnamed: 0')
-new_df = pd.read_csv('norm_table.csv', index_col='Unnamed: 0')
-# new_df = pd.DataFrame([], index=df.index, columns=df.columns)
-#
-#
-# def normilize(x, MIN_p, MAX_p):
-#     return (x - MIN_p) / (MAX_p - MIN_p)
-#
-#
-# for col in df.columns:
-#     MAX_one = max(df[col])
-#     MAX = [MAX_one for i in range(45)]
-#
-#     MIN_one = min(df[col])
-#     MIN = [MIN_one for i in range(45)]
-#
-#     ser = pd.Series(np.array(list(map(normilize, df[col], MIN, MAX))), index=df.index)
-#     new_df[col] = ser
-# min_b1 = min(new_df.Y1)
-# min_b2 = min(new_df.Y2)
-# min_b3 = min(new_df.Y3)
-# min_b4 = min(new_df.Y4)
-#
-# max_b1 = max(new_df.Y1)
-# max_b2 = max(new_df.Y2)
-# max_b3 = max(new_df.Y3)
-# max_b4 = max(new_df.Y4)
-#
-# b1 = (max_b1 + min_b1)/2
-# b2 = (max_b2 + min_b2)/2
-# b3 = (max_b3 + min_b3)/2
-# b4 = (max_b4 + min_b4)/2
-#
-# b = []
-#
-# for i in range(45):
-#     min_b = min(new_df[['Y1', 'Y2', 'Y3', 'Y4']].iloc[i])
-#     max_b = max(new_df[['Y1', 'Y2', 'Y3', 'Y4']].iloc[i])
-#     b.append((min_b + max_b)/2)
-#
-# bo = np.array(b)
-# boo = pd.Series(bo, index=new_df.index)
-# new_df['b_row'] =  pd.Series(bo, index=new_df.index)
-#
-# b_1 = np.array([0.5 for i in range(45)])
-# b11 = pd.Series(b_1, index=new_df.index)
-# new_df['b_col'] =  pd.Series(b11, index=new_df.index)
 
+
+# new_df = pd.read_csv('table_51_b.csv', index_col='Unnamed: 0')
+# new_df = pd.read_csv('norm_table.csv', index_col='Unnamed: 0')
+df = pd.read_csv('test.csv', index_col='Unnamed: 0')
+new_df = pd.DataFrame([], index=df.index, columns=df.columns)
+
+
+def normilize(x, MIN_p, MAX_p):
+    return (x - MIN_p) / (MAX_p - MIN_p)
+
+
+for col in df.columns:
+    MAX_one = max(df[col])
+    MAX = [MAX_one for i in range(45)]
+
+    MIN_one = min(df[col])
+    MIN = [MIN_one for i in range(45)]
+
+    ser = pd.Series(np.array(list(map(normilize, df[col], MIN, MAX))), index=df.index)
+    new_df[col] = ser
+min_b1 = min(new_df.Y1)
+min_b2 = min(new_df.Y2)
+min_b3 = min(new_df.Y3)
+min_b4 = min(new_df.Y4)
+
+max_b1 = max(new_df.Y1)
+max_b2 = max(new_df.Y2)
+max_b3 = max(new_df.Y3)
+max_b4 = max(new_df.Y4)
+
+b1 = (max_b1 + min_b1)/2
+b2 = (max_b2 + min_b2)/2
+b3 = (max_b3 + min_b3)/2
+b4 = (max_b4 + min_b4)/2
+
+b_plus = []
+b_minus = []
+
+for i in range(45):
+    min_b = min(new_df[['Y1', 'Y2', 'Y3', 'Y4']].iloc[i])
+    max_b = max(new_df[['Y1', 'Y2', 'Y3', 'Y4']].iloc[i])
+    b_plus.append((min_b + max_b)/2)
+    b_minus.append((max_b-min_b)/2)
+
+bo = np.array(b_plus)
+new_df['b_row_plus'] =  pd.Series(bo, index=new_df.index)
+
+bo1 = np.array(b_minus)
+new_df['b_row_minus'] =  pd.Series(bo1, index=new_df.index)
+
+b_1 = np.array([0.5 for i in range(45)])
+b11 = pd.Series(b_1, index=new_df.index)
+new_df['b_col'] =  pd.Series(b11, index=new_df.index)
+new_df.to_csv('table_51_pl_min.csv')
 
 
 
@@ -121,24 +140,24 @@ for i in range(90):
 problem = pulp.LpProblem('0',pulp.LpMinimize)
 
 problem += l_facke, "Функция цели"
-for i in range(45):
+for i in range(32):
     problem += y[i] == (-lx11_1) * t_0(new_df['x11'].iloc[i]) - lx11_2 * t_1(new_df['x11'].iloc[i]) - lx11_3 * t_2(new_df['x11'].iloc[i]) + \
                        (-lx12_1) * t_0(new_df['x12'].iloc[i]) - lx12_2 * t_1(new_df['x12'].iloc[i]) - lx12_3 * t_2(new_df['x12'].iloc[i]) + \
                        (-lx21_1) * t_0(new_df['x21'].iloc[i]) - lx21_2 * t_1(new_df['x21'].iloc[i]) - lx21_3 * t_2(new_df['x21'].iloc[i]) + \
                        (-lx22_1) * t_0(new_df['x22'].iloc[i]) - lx22_2 * t_1(new_df['x22'].iloc[i]) - lx22_3 * t_2(new_df['x22'].iloc[i]) + \
                        (-lx31_1) * t_0(new_df['x31'].iloc[i]) - lx31_2 * t_1(new_df['x31'].iloc[i]) - lx31_3 * t_2(new_df['x31'].iloc[i]) + \
                        (-lx32_1) * t_0(new_df['x32'].iloc[i]) - lx32_2 * t_1(new_df['x32'].iloc[i]) - lx32_3 * t_2(new_df['x32'].iloc[i]) + \
-                       (-lx33_1) * t_0(new_df['x33'].iloc[i]) - lx33_2 * t_1(new_df['x33'].iloc[i]) - lx33_3 * t_2(new_df['x33'].iloc[i]) + l_facke - new_df['b_row'].iloc[i] >= 0, str(i + 1)
+                       (-lx33_1) * t_0(new_df['x33'].iloc[i]) - lx33_2 * t_1(new_df['x33'].iloc[i]) - lx33_3 * t_2(new_df['x33'].iloc[i]) + l_facke - new_df['Y1'].iloc[i] >= 0, str(i + 1)
 
 
-for i in range(45):
+for i in range(32):
     problem += y[i] == lx11_1 * t_0(new_df['x11'].iloc[i]) + lx11_2 * t_1(new_df['x11'].iloc[i]) + lx11_3 * t_2(new_df['x11'].iloc[i]) + \
                        lx12_1 * t_0(new_df['x12'].iloc[i]) + lx12_2 * t_1(new_df['x12'].iloc[i]) + lx12_3 * t_2(new_df['x12'].iloc[i]) + \
                        lx21_1 * t_0(new_df['x21'].iloc[i]) + lx21_2 * t_1(new_df['x21'].iloc[i]) + lx21_3 * t_2(new_df['x21'].iloc[i]) + \
                        lx22_1 * t_0(new_df['x22'].iloc[i]) + lx22_2 * t_1(new_df['x22'].iloc[i]) + lx22_3 * t_2(new_df['x22'].iloc[i]) + \
                        lx31_1 * t_0(new_df['x31'].iloc[i]) + lx31_2 * t_1(new_df['x31'].iloc[i]) + lx31_3 * t_2(new_df['x31'].iloc[i]) + \
                        lx32_1 * t_0(new_df['x32'].iloc[i]) + lx32_2 * t_1(new_df['x32'].iloc[i]) + lx32_3 * t_2(new_df['x32'].iloc[i]) + \
-                       lx33_1 * t_0(new_df['x33'].iloc[i]) + lx33_2 * t_1(new_df['x33'].iloc[i]) + lx33_3 * t_2(new_df['x33'].iloc[i]) + l_facke + new_df['b_row'].iloc[i] >= 0, str(i + 46)
+                       lx33_1 * t_0(new_df['x33'].iloc[i]) + lx33_2 * t_1(new_df['x33'].iloc[i]) + lx33_3 * t_2(new_df['x33'].iloc[i]) + l_facke + new_df['Y1'].iloc[i] >= 0, str(i + 46)
 
 
 
